fbf97c41a5345c0eab0c28cd1d84ffde5858fa65
Michal Panský <pansky.michal@gmail.com>
2022-12-12 11:25:18 +0100

    add validators

---
+++ src/validators/Error/InvalidBookingUUIDErrorValidator.ts
@@ -0,0 +1,27 @@
+import { Result } from "./../../types/Api";
+import { NumberValidator } from "./../ValidatorHelpers";
+import {
+  INVALID_BOOKING_UUID,
+  STATUS_BAD_REQUEST,
+} from "../../models/Error";
+import {
+  ModelValidator,
+  StringValidator,
+  ValidatorError,
+} from "../ValidatorHelpers";
+
+export class InvalidBookingUUIDErrorValidator implements ModelValidator {
+  public validate = (result: Result<any>): ValidatorError[] => {
+    return [
+      StringValidator.validate(`error`, result?.data?.error, {
+        equalsTo: INVALID_BOOKING_UUID,
+      }),
+      StringValidator.validate(`errorMessage`, result?.data?.errorMessage),
+      StringValidator.validate(`uuid`, result?.data?.uuid),
+      NumberValidator.validate(`status`, result?.response?.status, {
+        integer: true,
+        equalsTo: STATUS_BAD_REQUEST,
+      }),
+    ].flatMap((v) => (v ? [v] : []));
+  };
+}
---
+++ src/validators/Error/InvalidOptionIdErrorValidator.ts
@@ -0,0 +1,24 @@
+import { Result } from "./../../types/Api";
+import { NumberValidator } from "./../ValidatorHelpers";
+import { INVALID_OPTION_ID, STATUS_BAD_REQUEST } from "./../../models/Error";
+import {
+  ModelValidator,
+  StringValidator,
+  ValidatorError,
+} from "../ValidatorHelpers";
+
+export class InvalidOptionIdErrorValidator implements ModelValidator {
+  public validate = (result: Result<any>): ValidatorError[] => {
+    return [
+      StringValidator.validate(`error`, result?.data?.error, {
+        equalsTo: INVALID_OPTION_ID,
+      }),
+      StringValidator.validate(`errorMessage`, result?.data?.errorMessage),
+      StringValidator.validate(`optionId`, result?.data?.optionId),
+      NumberValidator.validate(`status`, result?.response?.status, {
+        integer: true,
+        equalsTo: STATUS_BAD_REQUEST,
+      }),
+    ].flatMap((v) => (v ? [v] : []));
+  };
+}
